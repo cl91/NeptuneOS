68d7299aff8d8cb889ba034ce7a07c6d00c84611
Chang Liu <cl91@tuta.io>
2020-03-05 22:46:56 -0500

    Add dummy initcpio

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-mkdir build
-cd build
-cmake ../private/ntsvc -DCMAKE_TOOLCHAIN_FILE=../sel4/llvm.cmake -DTRIPLE=i686-pc-none-elf -G Ninja
+mkdir -p build/{pe,elf,images}
+cd build/elf
+cmake ../../private/ntsvc -DCMAKE_TOOLCHAIN_FILE=../../sel4/llvm.cmake -DTRIPLE=i686-pc-none-elf -G Ninja
 ninja
+cd ../pe
+echo 'Hello, world!' > hello.txt
+echo 'hello.txt' > image-list
+cpio -o < image-list > initcpio
+objcopy --add-section initcpio=initcpio ../elf/ntsvc ../images/ntos
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64  -cpu Nehalem,-vme,+pdpe1gb,-xsave,-xsaveopt,-xsavec,-fsgsbase,-invpcid,enforce -m size=512M  -kernel build/kernel-ia32-pc99 -initrd build/ntsvc
+qemu-system-x86_64  -cpu Nehalem,-vme,+pdpe1gb,-xsave,-xsaveopt,-xsavec,-fsgsbase,-invpcid,enforce -m size=512M  -kernel build/elf/kernel-ia32-pc99 -initrd build/images/ntos
