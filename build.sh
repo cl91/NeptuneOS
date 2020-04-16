157937757c1ab37b4410fd83ee127ea54dae8340
Chang Liu <cl91@tuta.io>
2020-04-16 14:40:32 -0400

    Build boot iso

--- build.sh
+++ build.sh
@@ -14,3 +14,23 @@ echo 'hello.txt' > image-list
 cpio -o < image-list > initcpio
 objcopy --add-section initcpio=initcpio ../elf/ntsvc ../images/ntos
 cp ../elf/kernel-$SEL4_ARCH-pc99 ../images/kernel
+
+cd ..
+mkdir -p iso/boot/grub
+cp images/kernel iso/
+cp images/ntos iso/
+cat <<EOF > iso/boot/grub/grub.cfg
+set timeout=1
+menuentry 'seL4-ntos' --class fedora --class gnu-linux --class gnu --class os {
+    insmod all_video
+    insmod gzio
+    insmod part_msdos
+    insmod ext2
+    set root='(cd)'
+    echo 'Loading seL4 micro kernel'
+    multiboot /kernel
+    echo 'Loading ntsvc...'
+    module /ntos
+}
+EOF
+grub-mkrescue -o boot.iso iso/
---
+++ docs/Dependencies.md
@@ -0,0 +1,12 @@
+Python dependencies
+===================
+
+jinja2
+future
+ply
+
+Boot ISO Tools
+==============
+grub
+xorriso
+mtools
