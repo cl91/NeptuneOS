1d420226e30af45b11b34541ebb433f319adc7d0
Chang Liu <cl91@tuta.io>
2020-05-11 13:03:39 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-qemu-system-i386  -cpu pentium3,-vme,-pdpe1gb,-xsave,-xsaveopt,-xsavec,-fsgsbase,-invpcid,enforce -m size=400M  -kernel build/images/kernel -initrd build/images/ntos,build/pe/initcpio $@
+qemu-system-i386  -cpu pentium3,-vme,-pdpe1gb,-xsave,-xsaveopt,-xsavec,-fsgsbase,-invpcid,enforce -m size=400M  -kernel build/images/kernel -initrd build/images/ntos $@
