3c17c46c36a81f9109dcfa00c472206d31754d86
Chang Liu <cl91@tuta.io>
2021-07-28 15:20:15 -0400

    rtl: Move ntos/rtl/string to rtl/crt/string
    Add preprocessor macros so that assembly files compile on both
    ELF and PE targets

--- private/ntdll/src/ldr/init.c
+++ private/ntdll/src/ldr/init.c
@@ -1,9 +1,18 @@
 #include <sel4/sel4.h>
+#include <nt.h>
+#include <string.h>
+
+static CHAR LdrpUninitializedArray[1024];
+
+static CHAR LdrpInitializedArray[1024] = "From initialized array.\n";
 
 void LdrpInitialize()
 {
 #ifdef CONFIG_DEBUG_BUILD
     seL4_DebugPutString("Hello, world from NT client!\n");
+    seL4_DebugPutString(LdrpInitializedArray);
+    memcpy(LdrpUninitializedArray, "From .bss array\n", sizeof("From .bss array\n"));
+    seL4_DebugPutString(LdrpUninitializedArray);
 #endif
 
     while (1);
