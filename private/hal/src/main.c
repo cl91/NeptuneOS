b67809ec82d8a0b1ab1b42bdeb418c92ae58d461
Chang Liu <cl91@tuta.io>
2021-10-17 15:54:55 -0400

    hal: Merge .tls section and .data section to save space.
    .tls section must come before .data and .bss sections.

--- private/hal/src/main.c
+++ private/hal/src/main.c
@@ -8,20 +8,24 @@ __thread seL4_IPCBuffer *__sel4_ipc_buffer;
 
 ULONG _tls_index;
 
-/* TLS raw template data start and end.
-   We use here pointer-types for start/end so that tls-data remains
-   aligned on pointer-size-width.  This seems to be required for
-   pe-loader. */
+/*
+ * TLS raw template data start and end.
+ *
+ * Note that we merge the .tls section and the .data section to save space.
+ * In doing so we need to make sure that the .tls section comes before
+ * the .data section, since clang generates code that expects that tls
+ * data are at the very beginning of a section. This is done by passing
+ * /merge:.data=.tls to the linker (note that the ordering matters).
+ *
+ * We use here pointer-types for start/end so that tls-data remains
+ * aligned on pointer-size-width.
+ */
 __declspec(allocate(".tls")) char *_tls_start = NULL;
 __declspec(allocate(".tls$ZZZ")) char *_tls_end = NULL;
 
-__declspec(allocate(".CRT$XLA")) PIMAGE_TLS_CALLBACK __xl_a = 0;
-__declspec(allocate(".CRT$XLZ")) PIMAGE_TLS_CALLBACK __xl_z = 0;
-
 const IMAGE_TLS_DIRECTORY _tls_used = {
   (ULONG_PTR) &_tls_start, (ULONG_PTR) &_tls_end,
-  (ULONG_PTR) &_tls_index, (ULONG_PTR) (&__xl_a+1),
-  (ULONG) 0, (ULONG) 0
+  (ULONG_PTR) &_tls_index, 0, 0, 0
 };
 
 static NTSTATUS IopCallDriverEntry(PDRIVER_OBJECT DriverObject)
