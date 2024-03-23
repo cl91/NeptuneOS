9c5deb36d41297a8982fa6a940506e14bed0731d
Dr. Chang Liu, PhD. <cl91tp@gmail.com>
2024-03-23 17:31:52 +0800

    WIP

--- private/ntos/src/io/file.c
+++ private/ntos/src/io/file.c
@@ -1,5 +1,4 @@
 #include "iop.h"
-#include "helpers.h"
 
 NTSTATUS IopFileObjectCreateProc(IN POBJECT Object,
 				 IN PVOID CreaCtx)
@@ -199,28 +198,6 @@ NTSTATUS NtOpenFile(IN ASYNC_STATE State,
 {
     NTSTATUS Status;
 
-    /* We haven't implemented file systems yet so we hard-code the logic
-     * for directory files. */
-    if (OpenOptions & FILE_DIRECTORY_FILE) {
-	POBJECT_DIRECTORY Dir = NULL;
-	Status = ObReferenceObjectByName(ObjectAttributes.ObjectNameBuffer,
-					 OBJECT_TYPE_DIRECTORY, NULL,
-					 !!(ObjectAttributes.Attributes & OBJ_CASE_INSENSITIVE),
-					 (POBJECT *)&Dir);
-	if (!NT_SUCCESS(Status)) {
-	    goto out;
-	}
-	assert(Dir != NULL);
-	Status = ObCreateHandle(Thread->Process, Dir, FileHandle);
-	ObDereferenceObject(Dir);
-    out:
-	if (IoStatusBlock != NULL) {
-	    IoStatusBlock->Status = Status;
-	    IoStatusBlock->Information = 0;
-	}
-	return Status;
-    }
-
     ASYNC_BEGIN(State, Locals, {
 	    IO_OPEN_CONTEXT OpenContext;
 	});
@@ -253,8 +230,40 @@ NTSTATUS NtReadFile(IN ASYNC_STATE State,
                     IN OPTIONAL PLARGE_INTEGER ByteOffset,
                     IN OPTIONAL PULONG Key)
 {
-    IO_SERVICE_PROLOGUE(State, Locals, FileObject, EventObject,
-			IoPacket, PendingIrp);
+    assert(Thread != NULL);
+    assert(Thread->Process != NULL);
+    NTSTATUS Status = STATUS_NTOS_BUG;
+
+    ASYNC_BEGIN(State, Locals, {
+	    PIO_FILE_OBJECT FileObject;
+	    PEVENT_OBJECT EventObject;
+	    PIO_PACKET IoPacket;
+	    PPENDING_IRP PendingIrp;
+	});
+
+    if (FileHandle == NULL) {
+	ASYNC_RETURN(State, STATUS_INVALID_HANDLE);
+    }
+    IF_ERR_GOTO(out, Status,
+		ObReferenceObjectByHandle(Thread, FileHandle, OBJECT_TYPE_FILE,
+					  (POBJECT *)&Locals.FileObject));
+    assert(Locals.FileObject != NULL);
+    assert(Locals.FileObject->DeviceObject != NULL);
+    assert(Locals.FileObject->DeviceObject->DriverObject != NULL);
+
+    if (EventHandle != NULL) {
+	IF_ERR_GOTO(out, Status,
+		    ObReferenceObjectByHandle(Thread, EventHandle, OBJECT_TYPE_EVENT,
+					      (POBJECT *)&Locals.EventObject));
+	assert(Locals.EventObject != NULL);
+    }
+
+    IF_ERR_GOTO(out, Status,
+		IopAllocateIoPacket(IoPacketTypeRequest, sizeof(IO_PACKET),
+				    &Locals.IoPacket));
+    assert(Locals.IoPacket != NULL);
+    Locals.IoPacket->Request.Device.Object = Locals.FileObject->DeviceObject;
+    Locals.IoPacket->Request.File.Object = Locals.FileObject;
 
     Locals.IoPacket->Request.MajorFunction = IRP_MJ_READ;
     Locals.IoPacket->Request.MinorFunction = 0;
@@ -265,12 +274,56 @@ NTSTATUS NtReadFile(IN ASYNC_STATE State,
 	Locals.IoPacket->Request.Read.ByteOffset = *ByteOffset;
     }
 
-    IO_SERVICE_EPILOGUE(out, Status, Locals, FileObject, EventObject,
-			IoPacket, PendingIrp, IoStatusBlock);
+
+    IF_ERR_GOTO(out, Status,
+		IopAllocatePendingIrp(Locals.IoPacket, Thread, &Locals.PendingIrp));
+    /* Note here the IO buffers in the driver address space are freed
+     * when the server receives the IoCompleted message, so we don't
+     * need to free them manually. */
+    Status = IopMapIoBuffers(Locals.PendingIrp, FALSE);
+    if (NT_SUCCESS(Status)) {
+	IopQueueIoPacket(Locals.PendingIrp, Thread);
+    } else {
+	IopFreePool(Locals.PendingIrp);
+	Locals.PendingIrp = NULL;
+	goto out;
+    }
+
+    /* For now every IO is synchronous. For async IO, we need to figure
+     * out how we pass IO_STATUS_BLOCK back to the userspace safely.
+     * The idea is to pass it via APC. When NtWaitForSingleObject
+     * returns from the wait the special APC runs and write to the
+     * IO_STATUS_BLOCK. We have reserved the APC_TYPE_IO for this. */
+
+    AWAIT(KeWaitForSingleObject, State, Locals, Thread,
+	  &Locals.PendingIrp->IoCompletionEvent.Header, FALSE, NULL);
+
+    /* This is the starting point when the function is resumed. */
+    if (IoStatusBlock != NULL) {
+	*IoStatusBlock = Locals.PendingIrp->IoResponseStatus;
+    }
+    Status = Locals.PendingIrp->IoResponseStatus.Status;
 
 out:
-    IO_SERVICE_CLEANUP(Status, Locals, FileObject,
-		       EventObject, IoPacket, PendingIrp);
+    /* The IO request has returned a error status. Clean up the
+       file object. */
+    if (!NT_SUCCESS(Status) && Locals.FileObject != NULL) {
+	ObDereferenceObject(Locals.FileObject);
+    }
+    if (!NT_SUCCESS(Status) && Locals.EventObject != NULL) {
+	ObDereferenceObject(Locals.EventObject);
+    }
+    if (Locals.PendingIrp == NULL & Locals.IoPacket != NULL) {
+	IopFreePool(Locals.IoPacket);
+    }
+    /* This will free the pending IRP and detach the pending irp
+     * from the thread. At this point the IRP has already been
+     * detached from the driver object, so we do not need to remove
+     * it from the driver IRP queue here. */
+    if (Locals.PendingIrp != NULL) {
+	IopCleanupPendingIrp(Locals.PendingIrp);
+    }
+    ASYNC_END(State, Status);
 }
 
 NTSTATUS NtWriteFile(IN ASYNC_STATE AsyncState,
