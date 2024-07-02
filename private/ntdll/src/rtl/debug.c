9f2ad54312f9f81aa1199ea77d8d5e80386ed0fb
Dr. Chang Liu, PhD. <cl91tp@gmail.com>
2024-07-02 16:27:55 +0800

    ntdll: Raise status when assertion is triggered

--- private/ntdll/src/rtl/debug.c
+++ private/ntdll/src/rtl/debug.c
@@ -41,42 +41,15 @@ ULONG DbgPrintEx(IN ULONG ComponentId,
     return 0;
 }
 
-static VOID DbgpPrintStackTrace()
-{
-    EXCEPTION_RECORD ExceptionRecord;
-    CONTEXT Context;
-
-    /* Capture the context */
-    RtlCaptureContext(&Context);
-
-    /* Create an exception record */
-    ExceptionRecord.ExceptionAddress = _ReturnAddress();
-    ExceptionRecord.ExceptionCode = STATUS_ASSERTION_FAILURE;
-    ExceptionRecord.ExceptionRecord = NULL;
-    ExceptionRecord.NumberParameters = 0;
-    ExceptionRecord.ExceptionFlags = EXCEPTION_NONCONTINUABLE;
-
-    /* Write the context flag */
-    Context.ContextFlags = CONTEXT_FULL;
-
-    EXCEPTION_POINTERS ExceptionInfo = {
-	.ExceptionRecord = &ExceptionRecord,
-	.ContextRecord = &Context
-    };
-    RtlpPrintStackTrace(&ExceptionInfo, FALSE);
-}
-
 VOID _assert(PCSTR str, PCSTR file, unsigned int line)
 {
     DbgPrint("Assertion %s failed at line %d of file %s\n",
 	     str, line, file);
-    DbgpPrintStackTrace();
     char buf[512];
     snprintf(buf, sizeof(buf), "Assertion %s failed at line %d of file %s\n",
 	     str, line, file);
     NtDisplayStringA(buf);
-    /* Loop forever */
-    while (1);
+    RtlRaiseStatus(STATUS_ASSERTION_FAILURE);
 }
 
 NTAPI ULONG RtlAssert(IN PVOID FailedAssertion,
@@ -87,14 +60,12 @@ NTAPI ULONG RtlAssert(IN PVOID FailedAssertion,
     DbgPrint("Assertion %s failed at line %d of file %s%s%s\n",
 	     (PCSTR)FailedAssertion, LineNumber, (PCSTR)FileName,
 	     Message ? ": " : "", Message ? Message : "");
-    DbgpPrintStackTrace();
     char buf[512];
     snprintf(buf, sizeof(buf), "Assertion %s failed at line %d of file %s%s%s\n",
 	     (PCSTR)FailedAssertion, LineNumber, (PCSTR)FileName,
 	     Message ? ": " : "", Message ? Message : "");
     NtDisplayStringA(buf);
-    /* Loop forever */
-    while (1);
+    RtlRaiseStatus(STATUS_ASSERTION_FAILURE);
     return 0;
 }
 
