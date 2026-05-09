35e06f90da5b2e8de3fb433f7fc306cef18e4196
Dr. Chang Liu, PhD. <cl91tp@gmail.com>
2026-05-09 17:03:00 +0800

    mkiso: compress image files to save space

--- mkiso.sh
+++ mkiso.sh
@@ -13,14 +13,22 @@ if [[ ${args[@]} =~ "amd64" ]]; then
     ARCH=amd64
 fi
 
+if [[ ${BUILD_TYPE} == Release ]]; then
+    KERNEL=kernel
+    NTOS=ntos
+else
+    KERNEL=kernel-stripped
+    NTOS=ntos-stripped
+fi
+
 BUILDDIR="build-$ARCH-${BUILD_TYPE,,}"
 IMAGEDIR="images-$ARCH-${BUILD_TYPE,,}"
 
 cd "$(dirname "$0")"
 cd $BUILDDIR
 mkdir -p iso/boot/grub
-cp $IMAGEDIR/kernel iso/
-cp $IMAGEDIR/ntos iso/
+gzip -c $IMAGEDIR/$KERNEL > iso/kernel.gz
+gzip -c $IMAGEDIR/$NTOS > iso/ntos.gz
 echo "set timeout=2" > iso/boot/grub/grub.cfg
 echo "menuentry 'Neptune OS $ARCH ($BUILD_TYPE Build)' --class fedora --class gnu-linux --class gnu --class os {" >> iso/boot/grub/grub.cfg
 cat <<EOF >> iso/boot/grub/grub.cfg
@@ -30,9 +38,9 @@ cat <<EOF >> iso/boot/grub/grub.cfg
     insmod ext2
     set gfxmode=1024x768
     echo 'Loading seL4 Microkernel...'
-    multiboot2 /kernel
+    multiboot2 /kernel.gz
     echo 'Loading NT Executive...'
-    module2 /ntos
+    module2 /ntos.gz
 }
 EOF
 grub-mkrescue -o boot.iso iso/
